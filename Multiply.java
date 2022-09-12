500f0828a62052032e25a156879f12808a7a70d1
Sarfraz Ahmed <ahmed.sarfraz@gmail.com>
2022-09-12 10:43:20 +0530

    added multiply

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+public class Multiply {
+
+    
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+        System.out.println("Multiplication is " + a*b);
+    }
+    
+}
