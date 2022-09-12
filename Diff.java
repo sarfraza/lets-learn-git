4bd17e85bc6d7ce875bf33ac03675665ffb2dac2
Sarfraz Ahmed <ahmed.sarfraz@gmail.com>
2022-09-12 11:07:29 +0530

    modified diff

--- Diff.java
+++ Diff.java
@@ -3,7 +3,7 @@ public class Diff {
     public static void main(String[] args) {
         int a = 10;
         int b = 5;
-        System.out.println("diff is" + (a-b));
+        System.out.println("diff is here" + (a-b));
     }
     
 }
